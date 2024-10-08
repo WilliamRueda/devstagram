eca8740052010a756b172bf33aead2777608b141
William Rueda <william.rueda@correounivalle.edu.co>
2024-10-08 18:17:59 -0500

    se arreglo el editar perfil

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\ImageManager;
 
 class PerfilController extends Controller
 {
@@ -15,5 +18,42 @@ class PerfilController extends Controller
     public function index(){
         return view('perfil.index');
     }
-    public function store(){}
+
+
+    public function store(Request $request){
+    
+      $this->validate($request, [
+        'username' => ['required','unique:users,username,' . auth()->user()->id,'min:3','max:20', 'not_in:editar-perfil', ],
+      ]);
+
+      if($request->imagen){
+       // 1. Recoger el archivo subido desde el formulario.
+    $imagen = $request->file('imagen');
+   
+    // 2. Generar un nombre único para la imagen usando UUID.
+    $nombreImagen = Str::uuid() . "." . $imagen->extension();
+
+    // 3. Instanciar ImageManager con GD como driver.
+    $manager = new ImageManager(['driver' => 'gd']);
+
+    // 4. Crear una instancia de la imagen usando el archivo cargado.
+    $image = $manager->make($imagen->getRealPath());
+    $image->fit(1000, 1000);
+    // 5. Definir la ruta donde se guardará la imagen.
+    $imagenPath = public_path('perfiles') . '/' . $nombreImagen;
+
+    // 6. Guardar la imagen en la ruta especificada.
+    $image->save($imagenPath);
+      }
+
+      /* Guardar cambios */
+      $usuario = User::find(auth()->user()->id);
+      $usuario->username = $request->username;
+      $usuario->image= $nombreImagen ?? '';
+
+      $usuario->save();
+
+      //Redireccionamos al user
+      return redirect()->route('posts.index', $usuario->username);
+    }
 }
--- resources/views/perfil/index.blade.php
+++ resources/views/perfil/index.blade.php
@@ -8,8 +8,8 @@
 @section('contenido')
     <div class="md:flex md:justify-center mt-10 ">
         <div class="md:w-1/2 bg-white shadow p-6">
-           <form action="" class="mt-10 md:mt-0">
-            
+           <form action="{{route('perfil.store',auth()->user())}}" method="POST" class="mt-10 md:mt-0" enctype="multipart/form-data">
+            @csrf
             <div class="mb-5">
                 <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">Username</label>
                 <input id="username" name="username" placeholder="tu nombre" class="border p-3 w-full rounded-lg   @error('username') border-red-500  @enderror "
@@ -21,13 +21,15 @@
             </div>
             <div class="mb-5">
                 <label for="imagen" class="mb-2 block uppercase text-gray-500 font-bold">Imagen Perfil</label>
-                <input id="imagen" name="imagen" placeholder="tu nombre" class="border p-3 w-full rounded-lg   @error('username') border-red-500  @enderror "
-                value="{{auth()->user()->username}}"   
-                type="file" >
-                    @error('imagen')
-                    <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{$message}}</p>
-                    @enderror
+                <input id="imagen" name="imagen" class="border p-3 w-full rounded-lg "
+                value=""   
+                type="file" 
+                accept=".jpg, .jpeg, .png"
+                >
+                 
             </div>
+            <input type="submit" value="Guardar Cambios"
+            class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg  ">
         </form> 
 
       
